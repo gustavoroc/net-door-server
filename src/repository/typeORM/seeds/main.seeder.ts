da012699f6c69eb1fe8bf7eed99b6bdbbdd3f39f
gustavo <gustavdrp@gmail.com>
2023-02-18 12:37:27 -0300

    feat: seeder now works with real random data

--- src/repository/typeORM/seeds/main.seeder.ts
+++ src/repository/typeORM/seeds/main.seeder.ts
@@ -1,4 +1,5 @@
-import { DataSource } from 'typeorm';
+import { faker } from '@faker-js/faker';
+import { DataSource, Repository } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { TORMEntityChat } from '../entities/chat.typeorm.entity';
 import { TORMEntityDoor } from '../entities/door.typeorm.entity';
@@ -13,25 +14,39 @@ export class MainSeeder implements Seeder {
     const doorRepository = dataSource.getRepository(TORMEntityDoor);
     const chatRepository = dataSource.getRepository(TORMEntityChat);
 
+    for (let x = 0; x < 10; x++) {
+      await this.commandToBeSeeded(
+        userRepository,
+        doorRepository,
+        chatRepository,
+      );
+    }
+
+    console.log('Data seeded successfully!');
+  }
+
+  async commandToBeSeeded(
+    userRepository: Repository<TORMEntityUser>,
+    doorRepository: Repository<TORMEntityDoor>,
+    chatRepository: Repository<TORMEntityChat>,
+  ) {
     const user = new TORMEntityUser();
-    user.name = 'John Doe';
-    user.email = 'johndoe@example.com';
+    user.name = faker.name.firstName();
+    user.email = faker.internet.email();
     user.createdAt = new Date();
     await userRepository.manager.save(user);
 
     const door = new TORMEntityDoor();
-    door.name = 'Door 1';
+    door.name = faker.name.firstName() + ' ' + 'Door';
     door.createdAt = new Date();
     door.users = [user];
     await doorRepository.manager.save(door);
 
     const chat = new TORMEntityChat();
-    chat.data = 'Hello, world!';
+    chat.data = faker.lorem.paragraph();
     chat.createdAt = new Date();
     chat.user = user;
     chat.door = door;
     await chatRepository.manager.save(chat);
-
-    console.log('Data seeded successfully!');
   }
 }
