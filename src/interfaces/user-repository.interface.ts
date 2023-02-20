075268896169e66b691ab8d1dec7b614cc6fcbfc
gustavo <gustavdrp@gmail.com>
2023-02-20 18:48:37 -0300

    feat: IUserRepository implemented

--- src/interfaces/user-repository.interface.ts
+++ src/interfaces/user-repository.interface.ts
@@ -1,1 +1,8 @@
-export interface IUserRepository {}
+import { CredentialsDTO } from 'src/dtos/credentials.dto';
+import { UserDTO } from 'src/dtos/user.dto';
+
+export interface IUserRepository {
+  createUser(user: UserDTO): Promise<boolean>;
+  findBy(query: string, data: string): Promise<UserDTO>;
+  checkPassword(credentials: CredentialsDTO): Promise<UserDTO>;
+}
